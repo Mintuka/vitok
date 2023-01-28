6280f76c7ca5469ce938b5d7b6a31624cbf14cc9
mintuka <mintuka2015@gmail.com>
2023-01-28 08:28:06 -0800

    update and delete controllers completed

--- backend/src/controllers/posts.ts
+++ backend/src/controllers/posts.ts
@@ -7,6 +7,7 @@ const router = express.Router();
 
 export const getPosts = async (req:Request, res:Response) => { 
     try {
+        console.log('get all')
         const postMessages = await PostMessage.find();
         res.status(200).json(postMessages);
     } catch (error) {
@@ -27,15 +28,19 @@ export const getPost = async (req:Request, res:Response) => {
 }
 
 export const createPost = async (req:Request, res:Response) => { 
+
+    req.body.creator = req.body.user._id
     const { title, message, selectedFile, creator, tags } = req.body;
     const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+    console.log('new', newPostMessage)
     try {
         await newPostMessage.save();
-        console.log(newPostMessage)
+        console.log('b-post',newPostMessage)
 
-        res.status(201).json(newPostMessage );
+        return res.status(201).json(newPostMessage );
     } catch (error) {
-        res.status(409).json({ message: error.message });
+        console.log('error',error)
+        return res.status(409).json({ message: error.message });
     }
 }
 
@@ -53,6 +58,7 @@ export const updatePost = async (req:Request, res:Response) => {
 }
 
 export const deletePost = async (req:Request, res:Response) => { 
+    console.log('delete backend')
     const { id } = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
@@ -75,4 +81,4 @@ export const likePost = async (req:Request, res:Response) => {
 }
 
 
-export default router;
+// export default router;
