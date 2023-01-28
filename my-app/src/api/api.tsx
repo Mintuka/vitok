18fc9c9299ac572f6044c5ab7f7c0dd8fdcca8ed
mintuka <mintuka2015@gmail.com>
2023-01-28 08:20:49 -0800

    feat(frontend): update and delete endpoints added

--- my-app/src/api/api.tsx
+++ my-app/src/api/api.tsx
@@ -1,8 +1,19 @@
 import axios from "axios"
+import { Action } from "../state/actions_interface/interfaces"
 
 const url = 'http://localhost:5000'
-
+const user: any = JSON.parse(localStorage.getItem('user') || '')   
+console.log(user) 
+const token = user[0].token
+const authAxios = axios.create({
+    baseURL: url,
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+})
 export const fetchPosts = () => axios.get(url+'/posts')
-export const createPosts = (newPost: Object) => axios.post(url+'/posts', newPost)
+export const createPosts = (newPost: Object) => authAxios.post(url+'/posts', newPost)
+export const updatePost = (_id: String) => authAxios.put<Action>(url+`/posts/${_id}`)
+export const deletePost = (_id: String) => authAxios.delete(url+`/posts/${_id}`)
 export const createUsers = (newUser: Object) => axios.post(url+'/users', newUser)
 export const logInUsers = (user: Object) => axios.post(url+'/users/login', user)
