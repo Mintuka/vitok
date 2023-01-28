39b52665a53400a4575df9ecee0c5fa48ea1e30a
mintuka <mintuka2015@gmail.com>
2023-01-28 08:29:02 -0800

    authentication middleware completed

---
+++ backend/src/middlewares/isAuthenticated.ts
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken'
+import { NextFunction, Request, Response } from "express"
+import User from '../models/User'
+import { secret } from '../services/createToken';
+
+interface JwtPayload {
+    _id: string;
+}
+
+export const isAuthenticated = async(req:Request, res:Response, next:NextFunction) => {
+    const { authorization } = req.headers
+    if (!authorization){
+        return res.status(401).json({error: 'Authorization tokend required'})
+    }
+    const token = authorization.split(' ')[1]
+    try{
+        const {_id} = jwt.verify(token, secret) as JwtPayload
+        req.body.user = await User.findOne({_id}).select('_id')
+        next()
+        return
+    }catch(err){
+        res.status(400).json({error: err})
+    }
+}
